6fbad4f62c01e52e5b8fc75d5082da16f6a03d08
Holden Oullette <hjo@uoregon.edu>
2014-11-13 12:35:14 -0800

    JavaScript function enabled and now the center compass spins with mouse movement

--- wvmf2014.js
+++ wvmf2014.js
@@ -1,18 +1,18 @@
-var circle = $(".circle");
-var circleCenterX = circle.width()/2;
-var circleCenterY = circle.height()/2;
-var offset = circle.offset();
-
-function mouse(e) {
-	var center_x = (offset.left) + ( circleCenterX );
-    var center_y = (offset.top) + ( circleCenterY );
-    var mouse_x = e.pageX; 
-    var mouse_y = e.pageY;
-
-    var radians = Math.atan2(mouse_x - center_x, center_y - mouse_y);
-
-    circle.css('-moz-transform', 'rotate('+radians+'rad)');
-    circle.css('-webkit-transform', 'rotate('+radians+'rad)');
-    circle.css('-o-transform', 'rotate('+radians+'rad)');
-    circle.css('-ms-transform', 'rotate('+radians+'rad)');
-}
+$(document).ready(function(){
+	var img = $('.image');
+	if(img.length > 0){
+		var offset = img.offset();
+		function mouse(evt){
+			var center_x = (offset.left) + (img.width()/2);
+			var center_y = (offset.top) + (img.height()/2);
+			var mouse_x = evt.pageX; var mouse_y = evt.pageY;
+			var radians = Math.atan2(mouse_x - center_x, mouse_y - center_y);
+			var degree = (radians * (180 / Math.PI) * -1) + 90; 
+			img.css('-moz-transform', 'rotate('+degree+'deg)');
+			img.css('-webkit-transform', 'rotate('+degree+'deg)');
+			img.css('-o-transform', 'rotate('+degree+'deg)');
+			img.css('-ms-transform', 'rotate('+degree+'deg)');
+		}
+	$(document).mousemove(mouse);
+	}
+});
